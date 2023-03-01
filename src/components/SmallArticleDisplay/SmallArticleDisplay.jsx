56a62a7ac4a0f511e2a068c27091385f44e5890b
andOrlando <bennettgillig@gmail.com>
2023-03-01 16:06:10 -0500

    Creates and populates MainPage and SmallArticleDisplay

---
+++ src/components/SmallArticleDisplay/SmallArticleDisplay.jsx
@@ -0,0 +1,30 @@
+import "./SmallArticleDisplay.css"
+import { useState } from "react"
+import React from "react"
+import { LoadingAnimation } from "../LoadingAnimation/LoadingAnimation"
+
+export class SmallArticleDisplay extends React.Component {
+  
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return <div className="smallarticledisplaycontainer">
+        <Image src={this.props.article.contentImg} style={{height: "160px"}}/>
+        <span id="title">{this.props.article.title}</span><br/>
+        <span id="subtitle">{this.props.article.subtitle}</span>
+        <span onClick={()=>console.log(this.props.article)}>{this.props.test}</span>
+      </div>
+  }
+}
+
+function Image({ src }) {
+  var [loading, setLoading] = useState(true)
+  function onload() { setLoading(false) }
+
+  return <div className="imagecontainer">
+      <div className="center-content" style={{display: loading || !src ? "" : "none"}}><LoadingAnimation/></div>
+      <img src={src} onLoad={onload} style={{height: "100%", width: "100%", objectFit: "cover", display: loading || !src ? "none" : ""}}></img>
+    </div>
+}
