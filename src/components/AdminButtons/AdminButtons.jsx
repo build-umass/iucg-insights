75eb0be4eecd83895e4e044d6f057cb0d099d322
andOrlando <bennettgillig@gmail.com>
2023-05-07 16:54:49 -0400

    Does editing stuff

--- src/components/AdminButtons/AdminButtons.jsx
+++ src/components/AdminButtons/AdminButtons.jsx
@@ -3,13 +3,14 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { deleteArticle } from "../../api"
 
-export default function AdminButtons({ id, editCallback }) {
+export default function AdminButtons({ id, editing, editCallback, saveCallback, cancelCallback }) {
   const [deleteStage, setDeleteStage] = useState(0)
   const navigate = useNavigate()
   let dog = false
 
   function handleDelete() {
     //TODO: figure out why dog doesn't work
+    //TODO: probably swap with useState or something
     if (dog) return
 
     if (deleteStage != 2) {
@@ -23,16 +24,26 @@ export default function AdminButtons({ id, editCallback }) {
   }
 
   return <div className="adminbuttons">
+      {editing ? <div style={{display: "flex", marginBottom: "10px"}}>
+        <CircleButton color="red" callback={cancelCallback}/>
+        <div className={"buttonstatecontainer center-content"}>
+          <div className="buttontext">Cancel</div>
+        </div>
+      </div> : null}
       <div style={{display: "flex", marginBottom: "10px"}}>
-        <CircleButton color="green" callback={editCallback}/>
-        <div className="center-content">Edit</div>
+        <CircleButton color={editing ? "red" : "green"}
+          callback={ editing ? saveCallback : editCallback}/>
+        <div className={"buttonstatecontainer center-content"}>
+          <div className="buttontext" style={{ opacity: !editing|0 }}>Edit</div>
+          <div className="buttontext" style={{ opacity: editing|0 }}>Save</div>
+        </div>
       </div>
       <div style={{display: "flex"}}>
         <CircleButton color={(["red", "green", "blue"])[deleteStage]} callback={handleDelete}/>
-        <div className={"deletecontainer center-content"}>
-          <div className="deletetext" style={{ opacity: (deleteStage == 0)|0 }}>Delete</div>
-          <div className="deletetext" style={{ opacity: (deleteStage == 1)|0 }}>Are you sure?</div>
-          <div className="deletetext" style={{ opacity: (deleteStage == 2)|0 }}>You're really really sure??</div>
+        <div className={"buttonstatecontainer center-content"}>
+          <div className="buttontext" style={{ opacity: (deleteStage == 0)|0 }}>Delete</div>
+          <div className="buttontext" style={{ opacity: (deleteStage == 1)|0 }}>Are you sure?</div>
+          <div className="buttontext" style={{ opacity: (deleteStage == 2)|0 }}>You're really really sure??</div>
         </div>
       </div>
     </div>
--- src/components/BlogDisplay/BlogDisplay.jsx
+++ src/components/BlogDisplay/BlogDisplay.jsx
@@ -1,39 +1,94 @@
 import "./BlogDisplay.css"
 import "../../common.css"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
 import React from "react";
+import ContentEditable from 'react-contenteditable';
 import { Remarkable } from "remarkable"
 import LoadingAnimation from "../LoadingAnimation/LoadingAnimation.jsx"
 import SmallerArticleDisplay from "../SmallerArticleDisplay/SmallerArticleDisplay.jsx"
-import { getArticle, getArticles } from "../../api"
+import AdminButtons from "../AdminButtons/AdminButtons"
+import { getArticle, getArticles, updateArticle } from "../../api"
 
 const md = new Remarkable();
 
-export default function BlogDisplay({ id }) {
+//functions for converting to and from html
+const toHTML=str=>str.includes("\n") ? str
+    .replaceAll(/(.*?)\n|(.+?)$|(?<=\n)()$/g,"<div>$1$2<br></div>")
+    .replace(/(.*?)<br>/, "$1")
+    : str
+const fromHTML=str=>str
+  .replaceAll(/<div>(.*?)<\/div>/g, "\n$1")
+  .replaceAll(/<br>/g, "")
+  .replace(/^\n/, "")
+  .replace(/&lt;/, "<")
+  .replace(/&gt;/, ">")
+
+export default function BlogDisplay() {
+  
+  const { id } = useParams()
+
   const [article, setArticle] = useState({
     title: null,
     author: null,
     authorImg: null,
     content: null,
     contentImg: null,
-    subtitle: null
+    subtitle: null 
   })
+  //keeps track of old article in the event of cancelling
+  const [oldArticle, setOldArticle] = useState(article)
   
-  useEffect(()=>{getArticle(id).then(setArticle)}, [])
+  const [html, setHTML] = useState("")
+  const [editing, setEditing] = useState(false)
 
-  return <div className="blogdisplay" style={{position: "relative"}}>
-    <Image src={article.contentImg}/>
-    <div className="contentcontainer">
-      <div className="content">
-        <Title title={article.title}/>
-        <Author author={article.author} authorImg={article.authorImg}/>
-        <Content markdown={md.render(article.content)}/>
+  //get inintial state of article via API and html via article
+  useEffect(()=>{getArticle(id).then(setArticle)}, [])
+  useEffect(()=>{
+    setHTML(toHTML(article.content || ""))
+    setOldArticle(article)
+  }, [editing])
+  
+  function saveCallback() {
+    setEditing(false)
+    updateArticle(id, article)
+  }
+  function cancelCallback() {
+    setEditing(false)
+    setArticle(oldArticle)
+  }
+
+  //once we've created all our components we can actually render our thing
+  return <>
+    <AdminButtons id={id}
+      editing={editing}
+      editCallback={()=>setEditing(true)}
+      cancelCallback={cancelCallback}
+      saveCallback={saveCallback}/>
+    <div className="blogdisplay" style={{position: "relative"}}>
+      <Image src={article.contentImg}/>
+      <div className={`contentcontainer ${editing ? "editing" : ""}`}>
+        <div className="content">
+          <Title article={article} editing={editing}
+            handleChange={e=>setArticle({...article, title: e.target.value})}/>
+          <Author article={article} editing={editing}
+            handleChange={e=>setArticle({...article, author: e.target.value})}/>
+          <Content
+            article={article} setArticle={setArticle}
+            html={html} setHTML={setHTML}
+            editing={editing}/>
+        </div>
+        {!editing ? <ReadMore/> : null}
       </div>
-      <ReadMore/>
     </div>
-  </div>
+  </>
 }
 
+/* Image component
+ * displays image or loading animation
+ * TODO: make work for authorimg
+ * TODO: use custom images
+ */
 function Image({ src }) {
   var [loading, setLoading] = useState(true)
   function onload() { setLoading(false) }
@@ -44,27 +99,68 @@ function Image({ src }) {
   </div>
 }
 
-function Content({ markdown }) {
-  return !markdown ?
-      <div className="center-content" style={{height: "100%"}}><LoadingAnimation/></div> :
-      <div dangerouslySetInnerHTML={{ __html: markdown }}></div>
+function Title({article, editing, handleChange}) {
+  return <ContentEditable
+      className="title"
+      html={article.title}
+      disabled={!editing}
+      onChange={handleChange}/>
 }
 
-function Title({ title }) {
-  return <div className="title">{title}</div>
+/* Content component
+ * Contains blog content and editing window
+ */
+function Content({article, setArticle, html, setHTML, editing}) {
+  //are we previewing
+  const [preview, setPreview] = useState(true)
+  useEffect(()=>{})
+  
+  const editref = useRef(null)
+  function handleChange(e) {
+    setHTML(e.target.value)
+    setArticle({...article, content: fromHTML(e.target.value)})
+  }
+  
+  return <>{editing ? <>
+      <div className="buttoncontainer">
+        <button className={preview ? "selected" : null} onClick={()=>setPreview(true)}>Preview</button>
+        <button className={preview ? null : "selected"} onClick={()=>setPreview(false)}>Edit</button>
+      </div>
+      <ContentEditable
+        className="editwindow"
+        style={{display: preview ? "none" : null}}
+        innerRef={editref}
+        html={html}
+        onChange={handleChange}/>
+      </> : null}
+      <div style={{display: preview || !editing ? null : "none"}}>
+        {!article.content ?
+          <div className="center-content" style={{height: "100%"}}><LoadingAnimation/></div> :
+          <div dangerouslySetInnerHTML={{ __html: md.render(article.content)}}></div>}
+      </div>
+    </>  
 }
 
-function Author({ author, authorImg }) {
+
+function Author({article, editing, handleChange}) {
+
   return <div className="author">
-      <img className="authorimg" src={authorImg}/>
-      <div className={"authortext center-content"}>{author}</div>
+      <img className="authorimg" src={article.authorImg}/>
+      <ContentEditable
+        className={"authortext center-content"}
+        html={article.author}
+        disabled={!editing}
+        onChange={handleChange} />
     </div>
 }
+
 
 function ReadMore() {
   const [articles, setArticles] = useState([])
   //TODO: get random articles instead of first 5 or try to get similar ones
   useEffect(()=>{getArticles().then(a=>setArticles(a.slice(0, 5)))}, [])
-  
+
   return <div className="readmore">{ articles.map(article => <SmallerArticleDisplay article={article} key={article._id}/>) }</div>
 }
+
+
