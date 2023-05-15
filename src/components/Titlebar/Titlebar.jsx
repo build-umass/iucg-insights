c9934bf5c6bd7ef0ac3022f46e9b090935c5f1da
andOrlando <bennettgillig@gmail.com>
2023-05-14 21:02:23 -0400

    Fixes icon switch for isAdmin

--- src/components/Titlebar/Titlebar.jsx
+++ src/components/Titlebar/Titlebar.jsx
@@ -30,7 +30,7 @@ export default function Titlebar({ setArticles }) {
       {cookies.isAdmin && <div onClick={() => navigator("/create")}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" fill="#FFFFFF"/></svg>
       </div>}
-      {cookies.isAdmin ?
+      {!cookies.isAdmin ?
       <div onClick={() => navigator("/login")}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" fill="#FFFFFF"/></svg>
       </div> :
