11ba295206dc1def0e88127d7a3dba477d0183ce
b-fenelon <bfenelon@umass.edu>
2023-05-15 05:57:59 -0400

    Cover photo upload to mongodb

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -14,13 +14,25 @@ function Create() {
     content: '',
     articles: [],
   });
+	
+  const [imgstate, setImgState] = useState({
+    url: '',
+    data: '',
+    type: '',
+  });
 
   const { title, subtitle, synopsis, author, content, articles } = state;
+  const { url, data, type } = imgstate;
   const navigate = useNavigate();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleUpload = (event) => {
+    const { name, value, files } = event.target;
+    setImgState({ [name]: value, data: files[0].name, type: files[0].type });
   };
 
   // call handleSubmit when the form is submitted
@@ -32,11 +44,24 @@ function Create() {
     }
 
     try {
-      const { data } = await axios.post('/api/articles',
-      { title, subtitle, synopsis, author, content });
-      setState((prevState) => ({ articles: [...prevState.articles, data],
+      const imgres = await axios.post('/api/images',
+      { url, data, type});
+      setImgState(() => ({ url: '', data: '', type: ''}));
+      //const contentImg = await axios.get(`/api/images/${imgres.data._id}`);
+      let getid;
+      if (imgres.data._id) {
+        getid = imgres.data;
+      } else {
+        getid = await axios.get(`/api/images/${imgres.data}`);
+	getid = getid.data;
+      }
+      console.log(getid);
+      const contentImg = getid._id;
+      const { log } = await axios.post('/api/articles',
+      { title, subtitle, synopsis, author, content, contentImg});
+      setState((prevState) => ({ articles: [...prevState.articles, log],
         title: '', subtitle: '', synopsis: '', author: '', content: '' }));
-      console.log(data)
+      console.log(log)
     
       // Navigate to main page
       navigate('/');
@@ -64,6 +89,8 @@ function Create() {
             <textarea id="content" name="content" value={content} onChange={handleChange}></textarea>
             <label htmlFor="author">Author</label>
             <input type="text" id="author" name="author" value={author} onChange={handleChange} />
+	    <label htmlFor="url">Cover Image</label>
+	    <input type="file" id="url" name="url" accept="image/*" value={url} onChange={handleUpload}/>
             <button type="submit">Submit</button>
           </form>
         </section>
