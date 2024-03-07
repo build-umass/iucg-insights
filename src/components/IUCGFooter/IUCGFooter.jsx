9f1102d7d64a5d02d6b2e1fad8024c6fc4a6d31b
maxwell3025 <amoinus@gmail.com>
2024-03-07 11:27:49 -0500

    footer

---
+++ src/components/IUCGFooter/IUCGFooter.jsx
@@ -0,0 +1,46 @@
+export default function IUCGFooter(){
+    return <div>
+        <div class="widget widget_text">
+            <h3 class="widgettitle title m_title m_title_ext text-custom">SUBSCRIBE TO OUR NEWSLETTER</h3>
+            <div class="textwidget">
+                <div class="wpcf7 js" id="wpcf7-f2903-o1" lang="en-US" dir="ltr">
+                    <form action="/#wpcf7-f2903-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
+                        <p>
+                            <span class="wpcf7-form-control-wrap" data-name="text-267">
+                                <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Your Name" value="" type="text" name="text-267" />
+                                <div style={{
+                                    position: "relative !important",
+                                    height: "0px !important",
+                                    width: "0px !important",
+                                    float: "left !important"
+                                 }} data-lastpass-icon-root="">
+                                </div>
+                            </span>
+                        </p>
+                        <p>
+                            <span class="wpcf7-form-control-wrap" data-name="email-970">
+                                <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your Email" value="" type="email" name="email-970" />
+                            </span>
+                        </p>
+                        <p>
+                            <span class="wpcf7-form-control-wrap" data-name="menu-583">
+                                <select class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false" name="menu-583">
+                                    <option value="I am a...">I am a...</option>
+                                    <option value="Student">Student</option>
+                                    <option value="Client">Client</option>
+                                    <option value="Recruiter">Recruiter</option>
+                                    <option value="Alumnus/Alumna">Alumnus/Alumna</option>
+                                    <option value="Faculty/Staff">Faculty/Staff</option>
+                                </select>
+                            </span>
+                        </p>
+                        <p>
+                            <input class="wpcf7-form-control has-spinner wpcf7-submit" type="submit" value="Sign Up" />
+                            <span class="wpcf7-spinner"></span>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+}
