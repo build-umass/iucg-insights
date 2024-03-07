ca34cb235e15ff3d904b41d068c8e246a222d28b
maxwell3025 <amoinus@gmail.com>
2024-03-07 11:45:05 -0500

    This works if CORS doesn't exist

--- src/components/IUCGFooter/IUCGFooter.jsx
+++ src/components/IUCGFooter/IUCGFooter.jsx
@@ -1,46 +1,280 @@
+import "./IUCGFooter.css"
 export default function IUCGFooter(){
-    return <div>
-        <div class="widget widget_text">
-            <h3 class="widgettitle title m_title m_title_ext text-custom">SUBSCRIBE TO OUR NEWSLETTER</h3>
-            <div class="textwidget">
-                <div class="wpcf7 js" id="wpcf7-f2903-o1" lang="en-US" dir="ltr">
-                    <form action="/#wpcf7-f2903-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
-                        <p>
-                            <span class="wpcf7-form-control-wrap" data-name="text-267">
-                                <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Your Name" value="" type="text" name="text-267" />
-                                <div style={{
-                                    position: "relative !important",
-                                    height: "0px !important",
-                                    width: "0px !important",
-                                    float: "left !important"
-                                 }} data-lastpass-icon-root="">
-                                </div>
-                            </span>
-                        </p>
-                        <p>
-                            <span class="wpcf7-form-control-wrap" data-name="email-970">
-                                <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your Email" value="" type="email" name="email-970" />
-                            </span>
-                        </p>
-                        <p>
-                            <span class="wpcf7-form-control-wrap" data-name="menu-583">
-                                <select class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false" name="menu-583">
-                                    <option value="I am a...">I am a...</option>
-                                    <option value="Student">Student</option>
-                                    <option value="Client">Client</option>
-                                    <option value="Recruiter">Recruiter</option>
-                                    <option value="Alumnus/Alumna">Alumnus/Alumna</option>
-                                    <option value="Faculty/Staff">Faculty/Staff</option>
-                                </select>
-                            </span>
-                        </p>
-                        <p>
-                            <input class="wpcf7-form-control has-spinner wpcf7-submit" type="submit" value="Sign Up" />
-                            <span class="wpcf7-spinner"></span>
-                        </p>
-                    </form>
+    return <footer
+    id="footer"
+    className="site-footer"
+    role="contentinfo"
+    itemScope="itemscope"
+    itemType="https://schema.org/WPFooter"
+    >
+    <div className="container">
+        <div className="row">
+        <div className="col-sm-5">
+            <div id="text-8" className="widget widget_text">
+            <h3 className="widgettitle title m_title m_title_ext text-custom">
+                SUBSCRIBE TO OUR NEWSLETTER
+            </h3>{" "}
+            <div className="textwidget">
+                <div
+                className="wpcf7 no-js"
+                id="wpcf7-f2903-o1"
+                lang="en-US"
+                dir="ltr"
+                >
+                <div className="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true" /> <ul />
+                </div>
+                <form
+                    action="/#wpcf7-f2903-o1"
+                    method="post"
+                    className="wpcf7-form init"
+                    aria-label="Contact form"
+                    noValidate="novalidate"
+                    data-status="init"
+                >
+                    <div style={{ display: "none" }}>
+                    <input type="hidden" name="_wpcf7" defaultValue={2903} />
+                    <input
+                        type="hidden"
+                        name="_wpcf7_version"
+                        defaultValue="5.7.7"
+                    />
+                    <input
+                        type="hidden"
+                        name="_wpcf7_locale"
+                        defaultValue="en_US"
+                    />
+                    <input
+                        type="hidden"
+                        name="_wpcf7_unit_tag"
+                        defaultValue="wpcf7-f2903-o1"
+                    />
+                    <input
+                        type="hidden"
+                        name="_wpcf7_container_post"
+                        defaultValue={0}
+                    />
+                    <input
+                        type="hidden"
+                        name="_wpcf7_posted_data_hash"
+                        defaultValue=""
+                    />
+                    </div>
+                    <p>
+                    <span
+                        className="wpcf7-form-control-wrap"
+                        data-name="text-267"
+                    >
+                        <input
+                        size={40}
+                        className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                        aria-required="true"
+                        aria-invalid="false"
+                        placeholder="Your Name"
+                        defaultValue=""
+                        type="text"
+                        name="text-267"
+                        />
+                    </span>
+                    </p>
+                    <div
+                    style={{
+                        position: "relative !important",
+                        height: "0px !important",
+                        width: "0px !important",
+                        float: "left !important"
+                    }}
+                    data-lastpass-icon-root=""
+                    />
+                    <p />
+                    <p>
+                    <span
+                        className="wpcf7-form-control-wrap"
+                        data-name="email-970"
+                    >
+                        <input
+                        size={40}
+                        className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email"
+                        aria-required="true"
+                        aria-invalid="false"
+                        placeholder="Your Email"
+                        defaultValue=""
+                        type="email"
+                        name="email-970"
+                        />
+                    </span>
+                    </p>
+                    <p>
+                    <span
+                        className="wpcf7-form-control-wrap"
+                        data-name="menu-583"
+                    >
+                        <select
+                        className="wpcf7-form-control wpcf7-select wpcf7-validates-as-required"
+                        aria-required="true"
+                        aria-invalid="false"
+                        name="menu-583"
+                        >
+                        <option value="I am a...">I am a...</option>
+                        <option value="Student">Student</option>
+                        <option value="Client">Client</option>
+                        <option value="Recruiter">Recruiter</option>
+                        <option value="Alumnus/Alumna">Alumnus/Alumna</option>
+                        <option value="Faculty/Staff">Faculty/Staff</option>
+                        </select>
+                    </span>
+                    </p>
+                    <p>
+                    <input
+                        className="wpcf7-form-control has-spinner wpcf7-submit"
+                        type="submit"
+                        defaultValue="Sign Up"
+                    />
+                    </p>
+                    <div className="wpcf7-response-output" aria-hidden="true" />
+                </form>
                 </div>
             </div>
+            </div>
         </div>
+        <div className="col-sm-4">
+            <div id="text-7" className="widget widget_text">
+            <h3 className="widgettitle title m_title m_title_ext text-custom">
+                GET IN TOUCH
+            </h3>{" "}
+            <div className="textwidget">
+                <p>
+                <strong>Email:</strong>
+                <br />
+                <a href="mailto:isenbergucg@umass.edu">isenbergucg@umass.edu</a>
+                </p>
+                <p>
+                <strong>Address:</strong>
+                <br />
+                Isenberg School of Management,
+                <br />
+                UMass Amherst, 121 Presidents Dr,
+                <br />
+                Amherst, MA 01003, USA
+                </p>
+                <p>
+                <a
+                    href="https://www.google.com/maps/place/Isenberg+School+of+Management,+UMass+Amherst/@42.386697,-72.5270817,17z/data=!3m1!4b1!4m5!3m4!1s0x89e6d2743fd14531:0xb563acd3e798a189!8m2!3d42.386697!4d-72.524893?hl=en"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <i className="glyphicon glyphicon-map-marker kl-icon-white" />{" "}
+                    Open in Google Maps
+                </a>
+                </p>
+            </div>
+            </div>
+        </div>
+        <div className="col-sm-3">
+            <div
+            id="wpcw_social-2"
+            className="widget wpcw-widgets wpcw-widget-social"
+            >
+            <h3 className="widgettitle title m_title m_title_ext text-custom">
+                STAY CONNECTED
+            </h3>
+            <ul>
+                <li className="no-label">
+                <a
+                    href="https://www.linkedin.com/company/the-isenberg-consulting-group"
+                    target="_blank"
+                    title="Visit Isenberg Undergraduate Consulting Group on LinkedIn"
+                >
+                    <span className="fa fa-2x fa-linkedin">\f0e1</span>
+                </a>
+                </li>
+            </ul>
+            </div>
+            <div id="text-10" className="widget widget_text">
+            <h3 className="widgettitle title m_title m_title_ext text-custom">
+                USER PORTAL
+            </h3>{" "}
+            <div className="textwidget">
+                <p>
+                <a href="https://umass.account.box.com/login">
+                    <noscript>
+                    &lt;img class="alignnone wp-image-2906 size-full"
+                    src="https://www.isenbergconsulting.org/wp-content/uploads/2018/10/user.png"
+                    alt="" width="24" height="24" /&gt;
+                    </noscript>
+                    <img
+                    className="lazy alignnone wp-image-2906 size-full entered loaded"
+                    src="https://www.isenbergconsulting.org/wp-content/uploads/2018/10/user.png"
+                    data-src="https://www.isenbergconsulting.org/wp-content/uploads/2018/10/user.png"
+                    alt=""
+                    width={24}
+                    height={24}
+                    data-ll-status="loaded"
+                    />
+                </a>
+                </p>
+            </div>
+            </div>
+        </div>
+        </div>
+        {/* end row */}
+        <div className="row">
+        <div className="col-sm-12">
+            <div className="bottom site-footer-bottom clearfix">
+            <div className="zn_footer_nav-wrapper">
+                <ul id="menu-footer-menu-bottom" className="footer_nav">
+                <li
+                    id="menu-item-1801"
+                    className="menu-item menu-item-type-custom menu-item-object-custom menu-item-1801"
+                >
+                    <a href="#">DISCLAIMER</a>
+                </li>
+                <li
+                    id="menu-item-1802"
+                    className="menu-item menu-item-type-custom menu-item-object-custom menu-item-1802"
+                >
+                    <a href="#">SUPPORT POLICY</a>
+                </li>
+                <li
+                    id="menu-item-1803"
+                    className="menu-item menu-item-type-custom menu-item-object-custom menu-item-1803"
+                >
+                    <a href="#">LEGAL</a>
+                </li>
+                </ul>
+            </div>
+            <div className="copyright footer-copyright">
+                <a
+                href="https://www.isenbergconsulting.org"
+                className="footer-copyright-link"
+                >
+                <noscript>
+                    &lt;img class="footer-copyright-img"
+                    src="https://www.isenbergconsulting.org/wp-content/uploads/2018/10/logo_bottom_hover.png"
+                    width="300" height="55" alt="Isenberg Undergraduate Consulting
+                    Group" /&gt;
+                </noscript>
+                <img
+                    className="lazy footer-copyright-img entered loaded"
+                    src="https://www.isenbergconsulting.org/wp-content/uploads/2018/10/logo_bottom_hover.png"
+                    data-src="https://www.isenbergconsulting.org/wp-content/uploads/2018/10/logo_bottom_hover.png"
+                    width={300}
+                    height={55}
+                    alt="Isenberg Undergraduate Consulting Group"
+                    data-ll-status="loaded"
+                />
+                </a>
+                <p className="footer-copyright-text">
+                © 2018 | Isenberg Undergraduate Consulting Group | Designed By{" "}
+                <a href="https://www.sociolus.com/">Sociolus.</a>
+                </p>{" "}
+            </div>
+            {/* end copyright */}
+            </div>
+            {/* end bottom */}
+        </div>
+        </div>
+        {/* end row */}
     </div>
+    </footer>
 }
