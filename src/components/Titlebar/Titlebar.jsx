db3b0305a0fd2235addaab72ed31469c6cdd0548
maxwell3025 <amoinus@gmail.com>
2024-03-10 19:20:26 -0400

    Latest Changes

--- src/components/Titlebar/Titlebar.jsx
+++ src/components/Titlebar/Titlebar.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useCookies } from "react-cookie";
 import { searchArticle } from "../../api"
+import 'material-symbols';
 
 export default function Titlebar({ setArticles }) {
   const [cookies, , removeCookie] = useCookies(['myCookie']);
@@ -17,16 +18,16 @@ export default function Titlebar({ setArticles }) {
   };
   
   return <div className="titlebar">
-    <div className={"title center-content"}>IUCG</div>
+    <div className={"title center-content"}>
+      <img src="logo_light.png" alt="Isenberg Undergraduate Consulting Group"></img>
+    </div>
     <div className={"nav center-content"}>
+      <span className="material-symbols-outlined">search</span>
       <input
         type="search"
         value={search}
         onChange={e=>{setSearch(e.target.value)}}
         placeholder="Search"></input>
-      <div onClick={handleSubmit}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" fill="#FFFFFF"/></svg>
-      </div>
       {cookies.isAdmin && <div onClick={() => navigator("/create")}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" fill="#FFFFFF"/></svg>
       </div>}
