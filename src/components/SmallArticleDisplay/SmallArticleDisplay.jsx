c1016935ddf91159c6610f513cbcfb22a85553b0
maxwell3025 <amoinus@gmail.com>
2024-04-20 18:31:35 -0400

    Fixed Dots

--- src/components/SmallArticleDisplay/SmallArticleDisplay.jsx
+++ src/components/SmallArticleDisplay/SmallArticleDisplay.jsx
@@ -51,7 +51,7 @@ export default function SmallArticleDisplay({ article, removeCallback }) {
       <div className="date">{article.created.substring(0,10)}</div>
 
     </div>
-    {cookies.isAdmin && <>
+    {cookies.loginToken && <>
     <svg className="dotdotdot" ref={dotdotdot} onClick={toggleDots} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect width="100" height="100"/> <circle cx="80" cy="50" r="10" fill="#C8CAD1"/> <circle cx="50" cy="50" r="10" fill="#C8CAD1"/> <circle cx="20" cy="50" r="10" fill="#C8CAD1"/></svg>
     <div className={"buttons hide"} ref={dropdown}>
       <div onClick={navEditArticle} style={{"--offset": 1}}>
