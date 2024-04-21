13438a00cecd273c459faa1225659c9ce32b0237
andOrlando <bennettgillig@gmail.com>
2024-04-21 18:39:20 -0400

    Makes article creation pretty

--- src/components/Titlebar/Titlebar.jsx
+++ src/components/Titlebar/Titlebar.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from "react-router-dom"
 import 'material-symbols';
 import SearchPage from "../SearchPage/SearchPage";
 
-export default function Titlebar() {
+export default function Titlebar({ nosearch }) {
+
   const [isSearching, setIsSearching] = useState(false);
   const navigate = useNavigate()
  
@@ -12,7 +13,7 @@ export default function Titlebar() {
     <div onClick={() => navigate("/")} className={"title center-content"}>
       <img src="/logo_light.png" alt="Isenberg Undergraduate Consulting Group"></img>
     </div>
-    <div className={"nav center-content"}>
+    <div className={"nav center-content"} style={{display: nosearch ? "none" : undefined}}>
       <div
       className="material-symbols-outlined"
       onClick={() => setIsSearching(true)}
