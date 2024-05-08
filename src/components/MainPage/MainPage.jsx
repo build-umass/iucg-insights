cab0f34beb5f7e74baba70cf7228c465c9e67220
andOrlando <bennettgillig@gmail.com>
2024-05-08 18:44:17 -0400

    Carousel

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -30,17 +30,20 @@ export default function MainPage() {
   return <div className="mainpage">
     <Titlebar setArticles={setArticles} />
 
-    <div className="hero-banner">
-      <h1>IUCG INSIGHTS</h1>
-      At IUCG we value the insights we gain through industry experience.
-      Below you can view our Industry Reports, Case Studies, and Client Projects.
-      We hope you enjoy learning about our insights.
-    </div>
+      <div className="hero-banner">
+        <h1>IUCG INSIGHTS</h1>
+        At IUCG we value the insights we gain through industry experience.
+        Below you can view our Industry Reports, Case Studies, and Client Projects.
+        We hope you enjoy learning about our insights.
+        <div className="linkedin-logo">
+          <a href="https://www.linkedin.com/company/iucg/" />
+        </div>
+      </div> 
 
     <FeaturedInsights></FeaturedInsights>
 
-    <SearchSection articles={articles} removeCallBack={article => remove(article)} />
+      <SearchSection articles = {articles} removeCallback={article=>remove(article)} />
 
-    <IUCGFooter />
+      <IUCGFooter />
   </div>
 }
